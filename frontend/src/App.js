81179b0d0de365269b9ed2051683b3dae194ffb5
DungTien <adpttq113@gmail.com>
2023-04-25 22:00:20 +0700

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        <a href="/">amazona</a>      
+      </header>
+      <main>
+        list product
+      </main>
+    </div>
+  );
+}
+
+export default App;
